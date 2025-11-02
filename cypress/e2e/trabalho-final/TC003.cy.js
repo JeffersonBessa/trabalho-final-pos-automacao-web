1846171307cb1b281f12b7887ae63581edaa8826
JeffersonBessa <jefferson.bessa10@gmail.com>
2025-11-02 16:49:00 -0300

    developing test case 03

---
+++ cypress/e2e/trabalho-final/TC003.cy.js
@@ -0,0 +1,19 @@
+import HomePage from "../../pages/HomePage";
+import LoginPage from "../../pages/LoginPage";
+
+describe('Test Case 3', () => {
+    it('Login User with incorrect email and password', () => {
+        const user = {
+            email: 'invalid_email@example.com',
+            password: 'wrongPassword'
+        };
+
+        HomePage.visitHomePage();
+        HomePage.getLogo.should('be.visible');
+        HomePage.goToSignupLogin();
+        LoginPage.verifyLoginVisible();
+        LoginPage.login(user.email, user.password);
+        
+        LoginPage.verifyLoginError();
+    });
+});
